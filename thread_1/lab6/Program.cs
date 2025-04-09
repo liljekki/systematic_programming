a83477ab6f982bb423a7d096462854a8a8c82aff
liljekki <73700767+liljekki@users.noreply.github.com>
2025-04-09 19:03:08 +0300

    lab6

---
+++ thread_1/lab6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть шлях до директорії: ");
+        string path = Console.ReadLine();
+
+        if (Directory.Exists(path))
+        {
+            PrintDirectoryTree(path, "");
+        }
+        else
+        {
+            Console.WriteLine("Директорія не існує.");
+        }
+    }
+
+    static void PrintDirectoryTree(string path, string indent)
+    {
+        Console.WriteLine(indent + Path.GetFileName(path));
+
+        foreach (var dir in Directory.GetDirectories(path))
+        {
+            PrintDirectoryTree(dir, indent + "  ");
+        }
+
+        foreach (var file in Directory.GetFiles(path))
+        {
+            Console.WriteLine(indent + "  " + Path.GetFileName(file));
+        }
+    }
+}
